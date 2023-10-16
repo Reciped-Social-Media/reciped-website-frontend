95265c4a39b5e8ee903eb01afdef81b290feba65
Xirynx <64374129+Xirynx@users.noreply.github.com>
2023-10-17 01:10:01 +0200

    Add updated recipe card

--- src/components/Recipe/RecipeCard.jsx
+++ src/components/Recipe/RecipeCard.jsx
@@ -1,92 +1,127 @@
+import { useEffect, useRef, useState } from "react";
+import mealLogo from "../../assets/icons/meal.svg";
 import "./RecipeCard.css";
-import "../Post/PostModal.css";
-import cardIcon from "../../assets/icons/meal.svg";
-import infoIcon from "../../assets/icons/info.svg";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import { useState } from "react";
-import axios from "axios";
+import ShareIcon from "@mui/icons-material/Share";
+import { Close, Star, StarBorder } from "@mui/icons-material";
+import { postRequest } from "../../utils/request";
 import Recipe from "./Recipe";
-import CameraAltIcon from "@mui/icons-material/CameraAlt";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import PostModal from "../Post/PostModal";
-import { styled } from "@mui/material/styles";
 
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: "#ffffff",
-		},
-	},
-});
+const RecipeCard = ({ _recipe, _inCookbook }) => {
+	const [recipeData] = useState(_recipe);
+	const [id, setId] = useState(recipeData.id);
+	const [title, setTitle] = useState(recipeData.title);
+	const [ingredients, setIngredients] = useState(recipeData.ingredients);
+	const [directions, setDirections] = useState(recipeData.directions);
+	const [category] = useState(recipeData.category);
+	const [sharing, setSharing] = useState(false);
+	const [sharedError, setSharedError] = useState("");
 
-const StyledSwitch = styled(Switch)({
-	"& .MuiSwitch-thumb": {
-		"background-color": "#6c9eba",
-	},
-});
-
-
-const RecipeCard = (props) => {
-	const { title, recipeId, isPublic, category } = props.recipe;
-	const [checked, setChecked] = useState(isPublic);
 	const [showRecipeModal, setShowRecipeModal] = useState(false);
-	const [showPostModal, setShowPostModal] = useState(false);
+	const [showShareModal, setShowShareModal] = useState(false);
+	const recipeModal = useRef();
+	const shareModal = useRef();
+	const shareCategoryRef = useRef();
+	const shareCaptionRef = useRef();
 
-	const handleCheck = (event) => {
-		const isChecked = event.target.checked;
-		const accessToken = localStorage.getItem("accessToken");
-		axios.post("http://localhost:4000/cookbook/toggle-public", { checked: isChecked, recipeId }, {
-			headers: {
-				Authorisation: `Bearer ${accessToken}`,
-			},
-		});
-		setChecked(isChecked);
-	};
+	useEffect(() => {
+		if (showRecipeModal) {
+			recipeModal?.current?.showModal();
+		}
+		else {
+			recipeModal?.current?.close();
+		}
+		if (showShareModal) {
+			shareModal.current.showModal();
+		}
+		else {
+			shareModal.current.close();
+		}
+		setId(recipeData.id);
+		setTitle(recipeData.title);
+		setIngredients(recipeData.ingredients);
+		setDirections(recipeData.directions);
+	}, [
+		recipeData,
+		showRecipeModal,
+		showShareModal,
+	]);
 
-	const handleRecipeClick = () => {
-		setShowRecipeModal(prev => !prev);
-	};
-
-	const handlePostModalClick = () => {
-		setShowPostModal(prev => !prev);
+	const share = () => {
+		if (sharing) return;
+		setSharing(true);
+		if (shareCaptionRef.current.value === "") {
+			setSharedError("Caption cannot be empty.");
+			setSharing(false);
+			return;
+		}
+		const body = {
+			recipeId: id,
+			caption: shareCaptionRef.current.value,
+			category: shareCategoryRef.current.value,
+		};
+		postRequest("interaction/post", body)
+			.then((res) => {
+				setSharing(false);
+				if (res.status === 200) {
+					setShowShareModal(false);
+				}
+				else {
+					setSharedError(res.data.error);
+				}
+			})
+			.catch(() => {
+				setSharedError("Unknown error occurred!");
+				setSharing(false);
+			});
 	};
 
 	return (
-		<>
-			<div className="card-content">
-				<div className="post-section">
-					<ThemeProvider theme={theme}>
-						<CameraAltIcon color="primary" onClick={handlePostModalClick} className="card-icon"/>
-					</ThemeProvider>
-					<img src={infoIcon} width={25} onClick={handleRecipeClick} className="card-icon"></img>
-				</div>
-				<img src={cardIcon} className="recipe-icon"></img>
-				<text className="card-title">{title}</text>
-				<div className="card-category">{category}</div>
-				<div className="card-toggle">
-					<FormControlLabel control={<StyledSwitch onChange={handleCheck} checked={checked} disabled={true} color="primary"/>} label="Public" />
+		<div className="RecipeCard">
+			<div className="RecipeCard__empty" />
+			<div className="RecipeCard__image" onClick={() => setShowRecipeModal(true)}>
+				<img src={mealLogo}></img>
+			</div>
+			{category && <div className="RecipeCard__category">{category}</div>}
+			<div className="RecipeCard__title" onClick={() => setShowRecipeModal(true)}>
+				{title}
+			</div>
+			<div className="RecipeCard__interactions">
+				<div className="RecipeCard__share">
+					<ShareIcon style={{ color: "grey", cursor: "pointer" }} onClick={() => setShowShareModal(true)} />
+					<dialog className="RecipeCard__modal" ref={shareModal} onCancel={() => setShowShareModal(false)}>
+						<div className="RecipeCard__modal-close">
+							<Close onClick={() => setShowShareModal(false)} style={{ cursor: "pointer" }} />
+						</div>
+						<div className="RecipeCard__share-modal-title">
+							<h1>Share to the world!</h1>
+						</div>
+						<div className="RecipeCard__share-modal-input">
+							<textarea placeholder="Share what you think about this recipe..." maxLength={1024} minLength={1} ref={shareCaptionRef} />
+							<label>
+								Category:
+								<select defaultValue={category} ref={shareCategoryRef}>
+									<option value="All">All</option>
+									<option value="Breakfast">Breakfast</option>
+									<option value="Lunch">Lunch</option>
+									<option value="Dinner">Dinner</option>
+									<option value="Dessert">Dessert</option>
+								</select>
+							</label>
+							<div className="RecipeCard__share-modal-button">
+								<button disabled={sharing} onClick={share}>{sharing ? "Sharing..." : "Share"}</button>
+							</div>
+						</div>
+						{sharedError && <div className="RecipeCard__share-modal-error" style={{ color: "red" }}>{sharedError}</div>}
+					</dialog>
 				</div>
 			</div>
-			{
-				showRecipeModal && (
-					<>
-						<div className="RecipeCard__Blur" onClick={handleRecipeClick}/>
-						<div className="RecipeCard__Modal">
-							<Recipe recipeId={recipeId} cookbook={true}/>
-						</div>
-					</>
-				)
-			}
-			{ showPostModal && (
-				<>
-					<div className="Post_Blur" onClick={handlePostModalClick}/>
-					<div className="Post_Modal">
-						<PostModal recipe={props.recipe} handle={handlePostModalClick}/>
-					</div>
-				</>
-			)}
-		</>
+			{showRecipeModal && <dialog className="RecipeCard__modal" ref={recipeModal} onCancel={() => setShowRecipeModal(false)}>
+				<div className="RecipeCard__modal-close">
+					<Close onClick={() => setShowRecipeModal(false)} style={{ cursor: "pointer" }} />
+				</div>
+				<Recipe recipeId={id} title={title} ingredients={ingredients} directions={directions} />
+			</dialog>}
+		</div>
 	);
 };
 
