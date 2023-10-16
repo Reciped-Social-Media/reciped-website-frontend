6f6f54e09d0b5b594d4a4d12c3564ace4696b9da
Xirynx <64374129+Xirynx@users.noreply.github.com>
2023-10-17 01:11:13 +0200

    Updated explore page code

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,134 +1,42 @@
-import { useLoaderData } from "react-router-dom";
-import "./Explore.css";
-import PostTopBar from "../components/Post/PostTopBar";
-import PostRow from "../components/Post/PostRow";
+import { useEffect, useState } from "react";
+import PostRow from "../components/Explore/PostRow";
 import fireIcon from "../assets/icons/Fire.svg";
-import checkIcon from "../assets/icons/Check.svg";
-import clockIcon from "../assets/icons/clock.svg";
-import dessertIcon from "../assets/icons/iceCream.svg";
-import sunIcon from "../assets/icons/Sun.svg";
-import pizzaIcon from "../assets/icons/Pizza.svg";
-import moonIcon from "../assets/icons/Moon.svg";
-import { useState } from "react";
-import Recipe from "../components/Recipe/Recipe";
-import Search from "../components/Search/Search";
-import Loader from "../components/Loader/Loader";
-import searchIcon from "../assets/icons/SearchBlue.svg";
+import { useLoaderData } from "react-router-dom";
+import RecipeSearchBar from "../components/Recipe/RecipeSearchBar";
+import "./Explore.css";
 
 const Explore = () => {
 	const data = useLoaderData();
-	const posts = data.posts;
-	const picks = data.picks;
-	const justIn = posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).slice(0, 10);
-	const whatsHot = posts.filter(rec => rec.rating >= 4);
-	const breakfast = posts.filter(rec => rec.category === "Breakfast");
-	const lunch = posts.filter(rec => rec.category === "Lunch");
-	const dinner = posts.filter(rec => rec.category === "Dinner");
-	const dessert = posts.filter(rec => rec.category === "Dessert");
-	const username = localStorage.getItem("username");
+	const [hotPosts, setHotPosts] = useState([]);
+	const [newPosts, setNewPosts] = useState([]);
+	const [curatedPosts, setCuratedPosts] = useState([]);
+	const [breakfastPosts, setBreakfastPosts] = useState([]);
+	const [lunchPosts, setLunchPosts] = useState([]);
+	const [dinnerPosts, setDinnerPosts] = useState([]);
+	const [dessertPosts, setDessertPosts] = useState([]);
 
-	const [showRecipeId, setShowRecipeId] = useState(null);
-	const [postId, setPostId] = useState(null);
-	const [recipeInteractionData, setRecipeInteractionData] = useState({});
-	const [showRecipeModal, setShowRecipeModal] = useState(false);
-	const [searchResult, setSearchResult] = useState(null);
-	const [isLoading, setIsLoading] = useState(false);
-
-
-	const handleRecipeClick = () => {
-		setShowRecipeModal(prev => !prev);
-	};
+	useEffect(() => {
+		setHotPosts(data.hot);
+		setNewPosts(data.new);
+		setCuratedPosts(data.curated);
+		setBreakfastPosts(data.breakfast);
+		setLunchPosts(data.lunch);
+		setDinnerPosts(data.dinner);
+		setDessertPosts(data.dessert);
+	}, []);
 
 	return (
-		<div className="explore-background">
-			<div className="explore-content">
-				<PostTopBar username={username} setSearchResult={setSearchResult} setIsLoading={setIsLoading}/>
-				{!searchResult && !isLoading && <><PostRow
-					recipes={whatsHot}
-					title="WHATS HOT"
-					icon={fireIcon}
-					setRecipeId={setShowRecipeId}
-					setPostId={setPostId}
-					setRecipeData={setRecipeInteractionData}
-					handleRecipeClick ={handleRecipeClick}
-				/>
-				<PostRow
-					recipes={justIn}
-					title="JUST IN"
-					icon={clockIcon}
-					setRecipeId={setShowRecipeId}
-					setPostId={setPostId}
-					setRecipeData={setRecipeInteractionData}
-					handleRecipeClick ={handleRecipeClick}
-				/>
-				<PostRow
-					recipes={picks}
-					title="OUR PICKS"
-					icon={checkIcon}
-					setRecipeId={setShowRecipeId}
-					setPostId={setPostId}
-					setRecipeData={setRecipeInteractionData}
-					handleRecipeClick ={handleRecipeClick}
-				/>
-				<PostRow
-					recipes={breakfast}
-					title="BREAKFAST"
-					icon={sunIcon}
-					setRecipeId={setShowRecipeId}
-					setPostId={setPostId}
-					setRecipeData={setRecipeInteractionData}
-					handleRecipeClick ={handleRecipeClick}
-				/>
-				<PostRow
-					recipes={lunch}
-					title="LUNCH"
-					icon={pizzaIcon}
-					setRecipeId={setShowRecipeId}
-					setPostId={setPostId}
-					setRecipeData={setRecipeInteractionData}
-					handleRecipeClick ={handleRecipeClick}
-				/>
-				<PostRow
-					recipes={dinner}
-					title="DINNER"
-					icon={moonIcon}
-					setRecipeId={setShowRecipeId}
-					setPostId={setPostId}
-					setRecipeData={setRecipeInteractionData}
-					handleRecipeClick ={handleRecipeClick}
-				/>
-				<PostRow
-					recipes={dessert}
-					title="DESSERT"
-					icon={dessertIcon}
-					setRecipeId={setShowRecipeId}
-					setPostId={setPostId}
-					setRecipeData={setRecipeInteractionData}
-					handleRecipeClick ={handleRecipeClick}
-				/></>}
-				{searchResult && !isLoading && <Search recipes={searchResult} setRecipeId={setShowRecipeId} handleRecipeClick={handleRecipeClick} setPostId={setPostId}/>}
-				{isLoading && <div className="search-wait"><div className="search-wait-title"><img src={searchIcon} width={30}></img><h1>Let's have a look...</h1></div><div className="loader"><Loader/></div></div>}
-				{showRecipeModal && (
-					<>
-						<div className="RecipeCard__Blur" onClick={handleRecipeClick} />
-						<div className="RecipeCard__Modal">
-							<Recipe
-								recipeId={showRecipeId}
-								postId={postId}
-								interactions={{
-									comments: recipeInteractionData.comments,
-									ratings: recipeInteractionData.ratings,
-									likes: recipeInteractionData.likes,
-									category: recipeInteractionData.category,
-									username: recipeInteractionData.username,
-								}}
-								handleRecipeClick={handleRecipeClick}
-							/>
-						</div>
-					</>
-				)}
-			</div>
+		<div className="Explore">
+			<div className="Explore__searchbar"><RecipeSearchBar /></div>
+			<PostRow title="What's Hot" icon={fireIcon} recipes={hotPosts}/>
+			<PostRow title="New Posts" icon={fireIcon} recipes={newPosts}/>
+			<PostRow title="Curated Posts" icon={fireIcon} recipes={curatedPosts}/>
+			<PostRow title="Breakfast" icon={fireIcon} recipes={breakfastPosts}/>
+			<PostRow title="Lunch" icon={fireIcon} recipes={lunchPosts}/>
+			<PostRow title="Dinner" icon={fireIcon} recipes={dinnerPosts}/>
+			<PostRow title="Dessert" icon={fireIcon} recipes={dessertPosts}/>
 		</div>
 	);
 };
+
 export default Explore;
