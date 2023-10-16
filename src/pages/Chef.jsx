9c848f262e4011e33088228b84dc895ad0a1574d
Xirynx <64374129+Xirynx@users.noreply.github.com>
2023-10-17 01:10:46 +0200

    Add placeholder code for chef site

--- src/pages/Chef.jsx
+++ src/pages/Chef.jsx
@@ -1,12 +1,15 @@
 import { useLoaderData } from "react-router-dom";
+import Recipe from "../components/Recipe/Recipe";
 
 const Chef = () => {
 	const data = useLoaderData();
 	return (
-		<>
+		<div>
 			<h1>CHEF</h1>
-			<p>{data}</p>
-		</>
+			{
+				data.map(recipe => <Recipe title={recipe.title} ingredients={recipe.ingredients} directions={recipe.directions} />)
+			}
+		</div>
 	);
 };
 export default Chef;
