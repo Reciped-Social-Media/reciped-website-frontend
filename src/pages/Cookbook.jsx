c829c9a94a46762a948a3ade72b9522f3148bc29
Xirynx <64374129+Xirynx@users.noreply.github.com>
2023-10-18 22:45:16 +0200

    Styling fixes

--- src/pages/Cookbook.jsx
+++ src/pages/Cookbook.jsx
@@ -5,17 +5,7 @@ import { useEffect, useRef, useState } from "react";
 import Recipe from "../components/Recipe/Recipe";
 import { Close } from "@mui/icons-material";
 import { postRequest } from "../utils/request";
-
-const AddedListInputItem = ({ item, index, removeItem }) => {
-	return (
-		<div className="AddedListInputItem__ListInputItem">
-			<div className="AddedListInputItem__ListInputItem-value">{item}</div>
-			{index >= 0 && (
-				<Close onClick={() => removeItem(index)} style={{ cursor: "pointer", color: "lightcoral" }}/>
-			)}
-		</div>
-	);
-};
+import AddedListInputItem from "../components/AddedListInputItem/AddedListInputItem";
 
 const AddRecipeForm = () => {
 	const [title, setTitle] = useState("");
